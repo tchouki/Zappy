7f907518e81671fe5b1aabba97355239e31796ce
Dayrion <toixd@live.fr>
2020-06-06 16:26:32 +0200

    Add IP/Port verification + connection

---
+++ Assets/Scripts/connectionServer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace zappy_test.include
+{
+    public class Connection
+    {
+        int port = -1;
+        public string team;
+        string machine;
+
+        IPEndPoint endpoint;
+        IPAddress serverIp;
+        Socket socket;
+
+        public string receiveMsg()
+        {
+            Byte[] bytes = new byte[256];
+            int rec = 0;
+            string message;
+
+            rec = socket.Receive(bytes, bytes.Length, 0);
+            message = Encoding.ASCII.GetString(bytes, 0, rec);
+            return message;
+        }
+
+        public void sendMsg(string request)
+        {
+            Byte[] bytes = Encoding.ASCII.GetBytes(request);
+
+            socket.Send(bytes, bytes.Length, 0);
+        }
+
+        public bool connect()
+        {
+            serverIp = IPAddress.Parse("127.0.0.1");
+            endpoint = new IPEndPoint(serverIp, port);
+            try {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(endpoint);
+            } catch {
+                return false;
+            }
+            Console.Write(receiveMsg());
+            return true;
+        }
+
+        public Connection(string[] args)
+        {
+            int res;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "-p" && args[i + 1] != null && int.TryParse(args[i + 1], out res))
+                    port = int.Parse(args[i + 1]);
+                if (args[i] == "-n" && args[i + 1] != null)
+                    team = args[i + 1];
+                if (args[i] == "-h" && args[i + 1] != null)
+                    machine = args[i + 1];
+            }
+            if (port == -1 || team == null || machine == null)
+                throw new Exception("Error in command line arguments.");
+            if (!connect())
+                throw new Exception("Cannot connect to distant server.");
+        }
+    }
+}
--- Assets/Scripts/sceneController.cs
+++ Assets/Scripts/sceneController.cs
@@ -1,24 +1,65 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Net;
+using System.Linq;
 
 public class sceneController : MonoBehaviour
 {
+    private Button button;
+    private Text buttonText;
+    public InputField ip, port;
+    private Color32 colorBrown = new Color32(0x93, 0x6C, 0x38, 0xFF);
+
+    private static bool CheckIPValid(string ip)
+    {
+        if (String.IsNullOrWhiteSpace(ip))
+        {
+            return false;
+        }
+
+        string[] splitValues = ip.Split('.');
+        if (splitValues.Length != 4)
+        {
+            return false;
+        }
+
+        byte tempForParsing;
+
+        return splitValues.All(r => byte.TryParse(r, out tempForParsing));
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        button = GetComponent<Button>();
+        buttonText = GetComponentInChildren<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private bool setValidateColor(Image img, bool valid)
+    {
+        if (!valid)
+            img.color = Color.red;
+        else
+            img.color = colorBrown;
+        return (valid);
     }
 
     public void loadGameScene()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        bool validData;
+        Debug.Log("IP: " + ip.text);
+        Debug.Log("Port: " + port.text);
+        validData = setValidateColor(ip.image, CheckIPValid(ip.text));
+        validData = validData && setValidateColor(port.image, int.TryParse(port.text, out _));
+        if (validData)
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
     }
 
     public void loadMenuScene()
